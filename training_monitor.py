1a7621550526fc3a4de699a1dda4fe3ab4816475
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-03-13 23:16:20 +0530

    Add TrainingMonitor class for tracking training metrics

--- training_monitor.py
+++ training_monitor.py
@@ -1,1 +1,18 @@
+import torch
 
+
+class TrainingMonitor:
+
+    def __init__(self):
+        self.loss_history = []
+        self.gradient_norms = []
+
+    def record_loss(self, loss):
+        self.loss_history.append(loss)
+
+    def record_gradient_norm(self, model):
+
+        for p in model.parameters():
+            if p.grad is not None:
+                param_norm = p.grad.data.norm(2)
+                total_norm += param_norm.item() ** 2
