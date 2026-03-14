3461e225863418906e6acb9f821dddbc6271ff4f
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-03-14 23:33:27 +0530

    Helper utilities

--- utils.py
+++ utils.py
@@ -1,1 +1,13 @@
+import matplotlib.pyplot as plt
 
+def plot_loss(loss_history):
+
+    plt.plot(loss_history)
+
+    plt.xlabel("Iteration")
+
+    plt.ylabel("Loss")
+
+    plt.tile("Training Loss")
+
+    plt.show()
